415a84acd9eb76520aed5a9deb8aaa7e53808401
micahtilton <micah.tilton.20@gmail.com>
2024-11-15 17:18:35 +0000

    cleaned file structure

--- model/reload_database.py
+++ model/reload_database.py
@@ -65,13 +65,8 @@ faq_html_loader = HTMLDirectoryLoader("../web-scraper/faq-archive", faq_html_par
 faq_documents = list(faq_html_loader.lazy_load())
 faq_split_documents = text_splitter.split_documents(faq_documents)
 
-# vector_store_faq.add_documents([Document(page_content="This is a test document for duo mobile", metadata={"id": "test_doc_1"})])
-
 batch_size = 20  # Adjust this value based on your GPU memory
 for i in range(0, len(faq_split_documents), batch_size):
     print(f"Adding batch {i + 1} to {i + batch_size}")
     batch = faq_split_documents[i:i + batch_size]
     vector_store_faq.add_documents(batch)
-
-# print(f"{len(faq_split_documents)} faq docs in vector store")
-print(vector_store_faq.similarity_search("duo mobile"))
